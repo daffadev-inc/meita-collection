e73aa74abe049d14aad30dd39f438a6a658e3df2
daffadev-inc <ndangsaepudin@gmail.com>
2021-05-25 07:44:21 +0700

    update

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,40 +1,14 @@
 simpleCart({
-
-  checkout: {
-    // type: "PayPal",
-    // email: "you@yours.com",
-  },
-
-  // tax: 0.075,
-  // currency: "THB",
-
-  cartStyle: "table",
-
-  cartColumns: [
-    { attr: "name" , label: "Name" } ,
-    { attr: "price" , label: "Price", view: 'currency' } ,
-    { attr: "size" , label: "Size" } ,
-    { attr: "color" , label: "Color" } ,
-    { view: "decrement" , label: false , text: "- 1" } ,
-    { attr: "quantity" , label: "Qty" } ,
-    { view: "increment" , label: false , text: "+ 1" } ,
-    { attr: "total" , label: "SubTotal", view: 'currency' } ,
-    { view: "remove" , text: "Remove" , label: false }
-  ]
-
-});
-
-// simpleCart.currency({
-  // code: "THB",
-  // name: "Thai Baht",
-  // symbol: "&#3647;",
-  // delimiter: " ",
-  // decimal: ",",
-  // after: true,
-  // accuracy: 0
-// });
-
-//* Refresh cart once simpleCart is ready to listen.
-simpleCart.ready(function() {
-  simpleCart.update();
+	cartColumns: [
+{ attr: "thumb", label: false, view: "thumb" },
+{ attr: "name", label: false, view: "link" },
+{ view: "remove", text: false, label: !1 },
+{ attr: "color", label: 'Warna', view: "color" },
+{ attr: "tali", label: 'Model', view: "tali" },
+{ view: "decrement", label: false },
+{ attr: "quantity", label: 'Jumlah', view: "jumlah" },
+{ view: "increment", label: false },
+{ attr: "total", label: false, view: "currency" },
+],
+	currency: "IDR",
 });
